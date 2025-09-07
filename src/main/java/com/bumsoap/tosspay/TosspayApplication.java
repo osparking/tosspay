8caada0f544b63b17944bcce8d9b118c3dad4b44
BumTiger <jbpark03@gmail.com>
2025-09-08 03:05:56 +0900

    initial downloaded from start.spring.io

---
+++ src/main/java/com/bumsoap/tosspay/TosspayApplication.java
@@ -0,0 +1,13 @@
+package com.bumsoap.tosspay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosspayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TosspayApplication.class, args);
+	}
+
+}
