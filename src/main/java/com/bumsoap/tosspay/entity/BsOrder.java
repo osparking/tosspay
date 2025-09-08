92a22f90b173da6caaf1ae0808dd62cbe8ecc5f5
BumTiger <jbpark03@gmail.com>
2025-09-08 09:37:35 +0900

    order entity w/ 4 essential members

---
+++ src/main/java/com/bumsoap/tosspay/entity/BsOrder.java
@@ -0,0 +1,32 @@
+package com.bumsoap.tosspay.entity;
+
+import com.bumsoap.tosspay.util.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class BsOrder {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(nullable = false)
+  private BigDecimal amount;
+
+  @Column(name = "order_time", updatable = false, nullable = false,
+      columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private LocalDateTime orderTime = LocalDateTime.now(); // 주문일시
+
+  @Column(nullable = false)
+  private OrderStatus orderStatus;
+}
