84afe7e1b61c0c1534b801a3c7cafa8846dde75c
BumTiger <jbpark03@gmail.com>
2025-09-08 11:52:42 +0900

    toss payment entity class - 2nd half

--- src/main/java/com/bumsoap/tosspay/entity/TossPayment.java
+++ src/main/java/com/bumsoap/tosspay/entity/TossPayment.java
@@ -1,7 +1,11 @@
 package com.bumsoap.tosspay.entity;
 
+import com.bumsoap.tosspay.util.TossPaymentMethod;
+import com.bumsoap.tosspay.util.TossPaymentStatus;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -23,4 +27,21 @@ public class TossPayment {
   @OneToOne
   @JoinColumn(name = "order_id", nullable = false)
   private BsOrder order;
+
+  private long totalAmount;
+
+  @Enumerated(value = EnumType.STRING)
+  @Column(nullable = false)
+  private TossPaymentMethod tossPaymentMethod;
+
+  @Enumerated(value = EnumType.STRING)
+  @Column(nullable = false)
+  private TossPaymentStatus tossPaymentStatus;
+
+  @Column(nullable = false)
+  private LocalDateTime requestedAt;
+
+  private LocalDateTime approvedAt;
+
+  // 기타 메서드 생략..
 }
