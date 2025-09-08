7efb6fe6930d43c641218bb4dcfe318aa9ddee11
BumTiger <jbpark03@gmail.com>
2025-09-08 17:24:16 +0900

    common api response class - message and data

---
+++ src/main/java/com/bumsoap/tosspay/request/ApiResponse.java
@@ -0,0 +1,11 @@
+package com.bumsoap.tosspay.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ApiResponse {
+  private String message;
+  private Object data;
+}
